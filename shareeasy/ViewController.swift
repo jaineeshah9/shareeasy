13edd4b7bbbb6604f90ecc4f1a760d71b2efa40f
js12349 <jainee.shah@outlook.com>
2023-03-13 15:15:42 -0700

    Initial Commit

---
+++ shareeasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shareeasy
+//
+//  Created by Jainee S on 3/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
